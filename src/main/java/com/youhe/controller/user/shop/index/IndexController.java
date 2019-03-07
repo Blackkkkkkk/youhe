507ca64c4a227a42b5894c1a94bcb6ad6dcf6c07
liumeixiang <1073273818@qq.com>
2019-03-07 13:29:19 +0800

    1.首页根据商品id跳转到详情页

--- src/main/java/com/youhe/controller/user/shop/index/IndexController.java
+++ src/main/java/com/youhe/controller/user/shop/index/IndexController.java
@@ -23,7 +23,6 @@ import com.youhe.utils.shiro.ShiroUserUtils;
 import org.activiti.editor.language.json.converter.util.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
