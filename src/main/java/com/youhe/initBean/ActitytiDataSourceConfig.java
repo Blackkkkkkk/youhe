14f09823298f41e36c4983368465eb3cd01e5617
linkaihui <linkaihui2012@sina.com>
2019-03-13 13:13:08 +0800

    数据源配置优化

--- src/main/java/com/youhe/initBean/ActitytiDataSourceConfig.java
+++ src/main/java/com/youhe/initBean/ActitytiDataSourceConfig.java
@@ -1,20 +1,16 @@
 package com.youhe.initBean;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.spring.SpringProcessEngineConfiguration;
 import org.activiti.spring.boot.AbstractProcessEngineAutoConfiguration;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 
@@ -23,38 +19,18 @@ public class ActitytiDataSourceConfig extends AbstractProcessEngineAutoConfigura
 
     private static final Logger log = LogManager.getLogger(ActitytiDataSourceConfig.class);
 
-
-    @Bean(name = "activitiDataSource")
-    public DataSource activitiDataSource() {
-        log.info("activitiDataSource 初始化...");
-
-        DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setUrl("jdbc:mysql://localhost/activiti");
-        druidDataSource.setUsername("root");
-        druidDataSource.setPassword("root123");
-        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-
-        return druidDataSource;
-    }
-
-    /*
-    @Bean(name = "activitiDataSource")
-    @ConfigurationProperties(prefix = "spring.datasource.activiti")
-    public DataSource activitiDataSource() {
-        log.info("activitiDataSource 初始化...");
-        return new DruidDataSource();
-    }
-*/
+    @Resource
+    private DataSource dataSource;
 
     @Bean
     public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(activitiDataSource());
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
     public SpringProcessEngineConfiguration springProcessEngineConfiguration() {
         SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
-        configuration.setDataSource(activitiDataSource());
+        configuration.setDataSource(dataSource);
         configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
         configuration.setJobExecutorActivate(true);
         configuration.setTransactionManager(transactionManager());
