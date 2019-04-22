3b8181d13c30cf077bdecb1d17d103a990f8f410
linkaihui <linkaihui2012@sina.com>
2019-04-22 13:45:49 +0800

    系统数据库脚本sql文件

---
+++ SQL/shop_tables.sql
@@ -0,0 +1,138 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : dev
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : activiti
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 22/04/2019 13:35:14
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+
+-- ----------------------------
+-- Table structure for shop_commodity
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_commodity`;
+CREATE TABLE `shop_commodity`  (
+  `ID` bigint(50) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品名字',
+  `PRICE` int(100) NULL DEFAULT 0 COMMENT '价格\r\n',
+  `NUM` int(255) NULL DEFAULT 0 COMMENT '数量',
+  `DETAIL_PICTURE` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '详情图片路径',
+  `THUMBNAIL` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '缩略图',
+  `TOP` int(4) NULL DEFAULT 0 COMMENT '1 置顶 0非置顶',
+  `STATUS` int(4) NULL DEFAULT NULL COMMENT '1 上架 0下架',
+  `REGISTER` datetime(4) NULL DEFAULT NULL COMMENT '注册时间',
+  `ORDER_NUM` int(100) NULL DEFAULT NULL COMMENT '排序号',
+  `HOT_SALE` int(4) NULL DEFAULT NULL COMMENT '是否热卖 1 热卖  0 非热卖',
+  `IS_NEW_PRODUCT_ORDER_NUM` int(20) NULL DEFAULT NULL COMMENT '是否新品排序号',
+  `COMMODITY_ID` bigint(50) NULL DEFAULT NULL COMMENT '商品分类',
+  `DESCRIBE` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `AVAILABLE` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_commodity_role
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_commodity_role`;
+CREATE TABLE `shop_commodity_role`  (
+  `CID` int(36) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `CNAME` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `AVAILABLE` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  `PARENT_ID` int(20) NULL DEFAULT NULL COMMENT '上级角色ID',
+  `ORDER_NUM` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`CID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_commodity_type
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_commodity_type`;
+CREATE TABLE `shop_commodity_type`  (
+  `TID` bigint(50) NOT NULL COMMENT '类型编号',
+  `COLOR` varchar(68) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '颜色',
+  `SIZE` varchar(68) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '尺寸大小',
+  `COMMODITY_TYPE_ID` int(11) NULL DEFAULT NULL COMMENT '商品编号',
+  `PARENTID` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '上级角色ID',
+  `sort` bigint(11) NOT NULL COMMENT '排序',
+  PRIMARY KEY (`TID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_index_carousel_text
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_index_carousel_text`;
+CREATE TABLE `shop_index_carousel_text`  (
+  `ID` int(100) NOT NULL AUTO_INCREMENT,
+  `TEXT_1` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TEXT_2` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TEXT_3` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TEXT_4` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TEXT_5` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TEXT_6` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `TYPE` int(5) NULL DEFAULT NULL COMMENT '区分是第几张轮播图  value:1 2 3 ',
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_order
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_order`;
+CREATE TABLE `shop_order`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(11) NOT NULL COMMENT '用户UID',
+  `b_order_num` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '大订单号',
+  `total_price` int(11) NOT NULL DEFAULT 0 COMMENT '订单总金额：单位分',
+  `status` tinyint(2) NOT NULL DEFAULT 0 COMMENT '订单状态：0-已取消；30-待支付；60-已支付待发货；90-已发货',
+  `delivery_addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '收货地址',
+  `end_time` datetime(0) NULL DEFAULT NULL COMMENT '完成时间',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '商品订单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_order_detail`;
+CREATE TABLE `shop_order_detail`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `b_order_num` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '大订单号',
+  `commodity_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `s_order_num` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '小订单号',
+  `commodity_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品名称',
+  `price` int(11) NOT NULL DEFAULT 0 COMMENT '订单总金额：单位分',
+  `num` int(6) NOT NULL DEFAULT 0 COMMENT '商品数量',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
+  `create_time` datetime(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '订单明细表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for shop_picture
+-- ----------------------------
+DROP TABLE IF EXISTS `shop_picture`;
+CREATE TABLE `shop_picture`  (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `REGISTER_DATE` datetime(3) NULL DEFAULT NULL,
+  `TYPE` int(4) NULL DEFAULT NULL COMMENT '1.详情图，2缩略图',
+  `FILE_NAME` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件名称',
+  `FILE_TYPE` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '文件类型',
+  `SAVE_FILE_NAME` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '保存到服务器目录的文件名称',
+  `REPORTADDR` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '保存到服务器目录的文件全路径',
+  `PREVIEW_ID` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '照片ID',
+  `SHOP_ID` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '商品的ID',
+  `PICTURE_SIZE` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '照片大小',
+  `PAGEADDR` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
---
+++ SQL/sys_tables.sql
@@ -0,0 +1,196 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : dev
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : activiti
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 22/04/2019 13:35:14
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+
+-- ----------------------------
+-- Table structure for sys_department
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_department`;
+CREATE TABLE `sys_department`  (
+  `DEPT_ID` bigint(20) NOT NULL AUTO_INCREMENT,
+  `PARENT_ID` bigint(20) NULL DEFAULT NULL COMMENT '上级部门ID，一级部门为0',
+  `NAME` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '部门名称',
+  `ORDER_NUM` int(11) NULL DEFAULT NULL COMMENT '排序',
+  `STATUS` tinyint(4) NULL DEFAULT 0 COMMENT '状态 1 启用  0 禁用',
+  PRIMARY KEY (`DEPT_ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_department_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_department_permission`;
+CREATE TABLE `sys_department_permission`  (
+  `ID` varchar(36) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `SYS_DEPLOYMENT_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `SYS_PERMISSION_ID` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_function
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_function`;
+CREATE TABLE `sys_function`  (
+  `FUNCTION_ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '权限ID',
+  `PARENT_ID` int(11) NULL DEFAULT 0 COMMENT '权限父ID',
+  `FUNCTION_NAME` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限名',
+  `FUNCTION_URL` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限URL',
+  `FUNCTION_TYPE` tinyint(1) NULL DEFAULT 0 COMMENT '权限类型 1菜单 2功能',
+  `CREATE_TIME` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `SORT` int(11) NULL DEFAULT 0 COMMENT '排序',
+  PRIMARY KEY (`FUNCTION_ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_permission`;
+CREATE TABLE `sys_permission`  (
+  `PID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `PNAME` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '资源名称',
+  `TYPE` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '资源类型：menu,button,',
+  `URL` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '访问url地址',
+  `PERCODE` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '权限代码字符串',
+  `PARENTID` bigint(20) NULL DEFAULT NULL COMMENT '父结点id',
+  `PARENTIDS` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '父结点id列表串',
+  `SORTSRING` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '排序号',
+  `AVAILABLE` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  PRIMARY KEY (`PID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `RID` int(36) NOT NULL AUTO_INCREMENT,
+  `RNAME` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `AVAILABLE` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  `PARENT_ID` bigint(20) NULL DEFAULT NULL COMMENT '上级角色ID',
+  `ORDER_NUM` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`RID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role_function
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_function`;
+CREATE TABLE `sys_role_function`  (
+  `ROLE_ID` int(11) NULL DEFAULT 0 COMMENT '角色ID',
+  `FUNCTION_ID` int(11) NULL DEFAULT 0 COMMENT '权限ID'
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '角色权限关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role_permission`;
+CREATE TABLE `sys_role_permission`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `sys_role_id` int(20) NOT NULL COMMENT '角色id（管理员权限id）',
+  `sys_permission_id` int(20) NOT NULL COMMENT '权限id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `UID` int(36) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `USER_ACCOUNT` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
+  `USER_NAME` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
+  `USER_PASSWORD` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `SALT` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '盐',
+  `LOCKED` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账号是否锁定，1：锁定，0未锁定',
+  `REGISTER_DATE` datetime(3) NULL DEFAULT NULL COMMENT '注册时间',
+  `PHONE` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '电话号码',
+  `EMAIL` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ROLE_ID` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色',
+  `DEPARTMENT_ID` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '部门',
+  `ORDER_NUM` int(20) NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`UID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_department
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_department`;
+CREATE TABLE `sys_user_department`  (
+  `id` int(36) NOT NULL AUTO_INCREMENT,
+  `sys_user_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sys_department_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`  (
+  `id` int(36) NOT NULL AUTO_INCREMENT,
+  `sys_user_id` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `sys_role_id` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `passWord` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_sex` int(1) NULL DEFAULT NULL,
+  `nick_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `role_id` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Function structure for getChildLst
+-- ----------------------------
+DROP FUNCTION IF EXISTS `getChildLst`;
+delimiter ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `getChildLst`(rootId INT) RETURNS varchar(1000) CHARSET utf8
+    READS SQL DATA
+BEGIN
+  DECLARE sTemp VARCHAR(1000);
+
+  DECLARE sTempChd VARCHAR(1000);
+
+  SET sTemp = '$';
+
+  SET sTempChd =cast(rootId as CHAR);
+
+  WHILE sTempChd is not null DO
+
+    SET sTemp = concat(sTemp,',',sTempChd);
+
+    SELECT group_concat(id) INTO sTempChd FROM t_files where FIND_IN_SET(parent_id,sTempChd)>0;
+
+  END WHILE;
+
+  RETURN sTemp;
+
+END
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
