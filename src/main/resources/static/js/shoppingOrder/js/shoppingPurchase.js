1d738fcb61603ee312e34accaed1cfad89e00226
liumeixiang <1073273818@qq.com>
2019-05-16 15:03:12 +0800

    1.商城           优化

--- src/main/resources/static/js/shoppingOrder/js/shoppingPurchase.js
+++ src/main/resources/static/js/shoppingOrder/js/shoppingPurchase.js
@@ -1,4 +1,40 @@
+/**
+ * Created by xiaoqiang on 2019/1/10.
+ */
 
+//新增vue组件化
+Vue.component('tree', {
+    name: 'treeNode',
+    props: ['lists'],
+    template: `
+		<div>
+				  <template v-for="item in lists">
+
+                        <li v-if="item.children == null" class="list-group-item clearfix"><a :href="'/touristShop/commodityMenu?cid='+item.cid+''"><i
+                                class="fa fa-angle-right"></i>
+                            {{item.cname}}</a>
+                        </li>
+                        <li v-if="item.children != null && item.children.length >0"
+                            class="list-group-item clearfix dropdown">
+                            <a v-if="item.children != null && item.children.length >0" href="javascript:void(0);" style="padding: 0px;" :id="item.cid"> 
+                            <i v-if="item.parentId == 0" class="fa fa-angle-right"></i>
+                            <i v-if="item.parentId != 0" class="fa fa-circle" style="float: left;margin-right: 7px;font-size: 5px;position: relative;top: 7px;color: #949fae;"></i>
+                            {{item.cname}}<i class="fa fa-angle-down"></i></a>
+                            <ul class="dropdown-menu" v-if="item.children != null && item.children.length >0">
+                                <treeNode :lists="item.children">                     
+                                </treeNode>
+                            </ul>
+
+                        <li v-if="item.children != null && item.children.length ==0" class="list-group-item clearfix">
+                                <a :href="'/touristShop/commodityMenu?cid='+item.cid+''" style="padding: 0px;" :id="item.cid">
+                                <i v-if="item.parentId == 0" class="fa fa-angle-right"></i>
+                                  <i v-if="item.parentId != 0" class="fa fa-circle" style="float: left;margin-right: 7px;font-size: 5px;position: relative;top: 7px;color: #949fae;"></i>{{item.cname}}</a></li>
+                        </li>
+                
+                  </template>
+		</div>
+	`
+})
 Vue.component('treeHeadTwo', {
     name: 'treeNode',
     props: ['lists'],
@@ -44,36 +80,39 @@ Vue.component('treeHead', {
 		</div>
 	`
 })
+
+
 var vm = new Vue({
     el: '#wrapper',
     data: {
         shopList: [],
         carData: {
             items: 0,
-            prices: 0,
+            prices: 0
         },
-        cartPrices: {
-            shipping: 0, //运费
-
-            allPrice: 0 // 总费用
-        },
+        addcar: {
+            cartNum: 0,
+            remark: "",
+        },  // view 详情的
         menList: [],
-        lists: []
+        lists: [],
+        shopDetails: {},
+        view: {},
+        stocknum: 0,
+        stocktotal: 0,
+        minpic: []
     },
     created: function () {
 
-    },
+    }
+    ,
     mounted: function () {
 
-        this.initcart();
-
         _this = this;
-
+        $.ajaxSettings.async = false;
 
         $.get("/commodity/list", function (r) {
-
-
-            _this.menList = r
+            _this.menList = r;
         })
 
 
@@ -85,30 +124,15 @@ var vm = new Vue({
             cname: 'cname',
             rootId: 0
         }
-
         _this.lists = _this.toTreeData(data, attributes)
+        $.ajaxSettings.async = false;
 
 
-        // this. test();
-
-        /*
-        this.$nextTick(function () {
-           // console.log(this.$refs.input1[0])
-        })*/
-    },
+    }
+    ,
     methods: {
-        pay: function () {
-            // price =vm.showList.bigOrderCode;
-            debugger;
 
-            // $.get("/activiti/deploy?modelId=" + id, function (data,status) {
-            $.get("/order/payment", function (r) {
-                if(r.Status == 0){
-                    window.location.href = (r.url)
-                }else {
-                    layer.msg(r.msg, {icon: 2, time: 500});
-                }
-            })
+        pay: function () {
         },
         //把数据类型转成树格式
         toTreeData: function (data, attributes) {
@@ -153,19 +177,52 @@ var vm = new Vue({
 
             return tree;
         }
-        ,
-        cartView: function () {
+        ,   // 购物车数量增加
+        addCardNum: function (event, index) {
+            debugger
+            _this.addcar.cartNum++;
+
+
 
         }
+        ,// 购物车数量减少
+        delCardNum: function (event, index) {
+            if (_this.addcar.cartNum > 0) {
+                _this.addcar.cartNum--;
+            }
+        }
         ,
-        addCar: function (event) {
+        //立即购买
+        purchase: function () {
+           id=vm.view.id;
+            var cartNum=$("input[name='text']").val();
+            // id=vm.id;
+            debugger
+            if (id && id != 'null') {
+                window.location.href="/order/shoppingPurchase?id=" + id + "&cartNum=" +cartNum;
+            }
+            // url = "/touristShop/addCart?id=" + el.id + "&remark=" + _this.addcar.remark + "&cartNum=" + _this.addcar.cartNum;
+        }
+        ,
+        addCar: function (event, type) {
 
+            _this = this;
             //获取点击对象
             x = event.clientX  // 获取点击对象的x 轴
             y = event.clientY  // 获取点击对象的y 轴
+            console.log(event)
             var _this = this;
             var el = event.currentTarget;
-            $.get("/touristShop/addCart?id=" + el.id, function (r) {
+
+
+            //type == 2  是view 那边添加
+            var url = "/touristShop/addCart?id=" + el.id;
+
+            if (type == 2) {
+                url = "/touristShop/addCart?id=" + el.id + "&remark=" + _this.addcar.remark + "&cartNum=" + _this.addcar.cartNum;
+            }
+
+            $.get(url, function (r) {
                 _this.initcart(r.shopList);
                 if (r.Status == 1) {
                     top.layer.msg('加入购物车成功', {icon: 1, time: 1000, offset: [y + 'px', x + 'px']});
@@ -179,34 +236,17 @@ var vm = new Vue({
             })
         }
         ,
-        initcart: function (r) {
-            var _this = this;
-            if (r == null || r == undefined || r == '') {
-                $.ajax({
-                    type: "POST",
-                    url: "/touristShop/initCart",
-                    dataType: "json",
-                    async: false,//取消异步
-                    success: function (data) {
-                        r = data;
-                    }
-                });
-            }
-            var num = 0;
-            var prices = 0;   // 购物车显示的价格
 
+        initcart: function () {
+            id=vm.view.id;
+            var cartNum=$("input[name='text']").val();
+            var prices=$("input[name='prices']").val();
+            var _this = this;
             _this.shopList = [];
-            for (var index in r) {
-                num++;
-                //  num +=  r.shopList[index].pirce
-                prices += r[index].pirce * r[index].cartNum;
-                _this.shopList.push(r[index])
-            }
-            _this.carData.items = num;
+            _this.carData.items = cartNum;
             _this.carData.prices = prices;
-            _this.cartPrices.allPrice = prices + _this.cartPrices.shipping
-        }
-        ,
+            debugger
+        },
         delCart: function (event) {
 
             var x = event.clientX;
@@ -224,49 +264,40 @@ var vm = new Vue({
                 }
             })
         }
-        ,   // 购物车数量增加
-        addCardNum: function (event, index) {
-
-            var x = event.clientX;
-            var y = event.clientY;
+        ,
+        showView: function (id) {//这个只是给基本方法   还没用上ajax请求
 
             _this = this;
+            $.ajax({
+                type: "POST",
+                url: "/touristShop/viewList",
+                data: {"id": id},
 
-            $.get("/touristShop/carNumUD?id=" + this.shopList[index].id + "&carNumUD=1", function (r) {
-                if (r.Status == 1) {
-                    top.layer.msg(r.msg, {icon: 1, time: 1000, offset: [y + 'px', x + 'px']}, function () {
-                        _this.initcart(r.shopList)
-                    });
+                success: function (r) {
+                    if (r.Status == 1) {
+
+                        _this.view = r.shopList;
+                        _this.stocktotal = _this.view.num;
+                        _this.minpic = _this.view.thumbnail.split(',');
+                        console.log(_this.view);
+                    }
                 }
             })
-
-        }
-        ,// 购物车数量减少
-        delCardNum: function (event, index) {
-            var x = event.clientX;
-            var y = event.clientY;
-
-            _this = this;
-
-            if (this.shopList[index].cartNum != 0) {
-                $.get("/touristShop/carNumUD?id=" + this.shopList[index].id + "&carNumUD=-1", function (r) {
-                    if (r.Status == 1) {
-                        top.layer.msg(r.msg, {icon: 1, time: 400, offset: [y + 'px', x + 'px']}, function () {
-                            _this.initcart(r.shopList)
-                        });
-                    }
-                })
-            }
-        }
-        ,
-        goShop: function () {
-            window.location.href = ('/touristShop/index')
-        }
+            layer.open({
+                title: false,
+                type: 1,
+                area: ['747px', '580px'],
+                content: $('#product-pop-up'),//这可以写弹出框的html内容
+            });
+            /*
+            if (e.stopPropagation)
+                e.stopPropagation();
+            else
+                window.event.cancelBubble = true;*/
+        },
     }
 })
 
 jQuery(document).ready(function () {
-
-    //  vm.initcart();
-
+    vm.initcart();
 })
